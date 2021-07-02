4f1cca6e9252035475122061cd685dd37ef33ba4
erlendwtf <erlend@erlend.wtf>
2021-07-02 20:54:35 +0200

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,32 +1,25 @@
-function LineMsg ($msg)
-{
-    "*" * 50
-    Write-Host -ForegroundColor Green $msg
-    "*" * 50
-}
-
-LineMsg "Starting OSDCloud ZTI"
+Write-Host -Fore Green "//Starting OSDCloud ZTI"
 
 
 #Change Display Resolution for Virtual Machine
 if ((Get-MyComputerModel) -like "*Virtual*") {
-    LineMsg "Setting Display Resolution to 1600x"
-    Set-DisRes 1600
+Write-Host -Fore Green "//Setting Display Resolution to 1600x"
+    Set-DisRes 1920
 }
 
 #Make sure I have the latest OSD Content
-LineMsg "Updating OSD PowerShell Module"
+Write-Host -Fore Green "//Updating OSD PowerShell Module"
 Install-Module OSD -Force
 
 
-LineMsg "Importing OSD PowerShell Module"
+Write-Host -Fore Green "//Importing OSD PowerShell Module"
 Import-Module OSD -Force
 
 #Start OSDCloud ZTI the RIGHT way
-LineMsg "Starting OSDCloud"
-Start-OSDCloud -OSLanguage nb-no -OSBuild "21H2" -OSEdition Enterprise -ZTI -SkipAutoPilot
+Write-Host -Fore Green "//Starting OSDCloud"
+Start-OSDCloud -OSLanguage nb-no -OSBuild "21H1" -OSEdition Enterprise -ZTI -SkipAutoPilot
 
 #Restart from WinPE
-LineMsg "Restarting in 20 seconds!"
+Write-Host -Fore Green "//Restarting in 20 seconds!"
 Start-Sleep -Seconds 20
 wpeutil reboot
